8002ed693103cff68282bf5f142aa2c9a9b69a55
Vrunda Joshi <vrunda.inexture@gmail.com>
2022-02-21 15:45:22 +0530

    inital setup

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import React from 'react';
+import HomePage from './HomePage';
+
+const App = () => {
+  return (
+    <div className='App'>
+      <HomePage />
+    </div>
+  );
+};
+
+export default App;
